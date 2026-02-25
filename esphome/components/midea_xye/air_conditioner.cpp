361989b965dcf5943c4a38c2d94db775a6480b40
Matthew Drobnak <matthew@drobnak.com>
2026-02-25 17:25:33 +0000

    Revert "Try and account for defrost as well."
    This reverts commit f03ba9a302cc14ddfcb7d819cef95f5bed66d851.

--- esphome/components/midea_xye/air_conditioner.cpp
+++ esphome/components/midea_xye/air_conditioner.cpp
@@ -389,9 +389,8 @@ void AirConditioner::ParseResponse(uint8_t cmdSent) {
             update_property(this->target_temperature, (float) (RXData[RX_C0_BYTE_SET_TEMP] & 0xBF), need_publish);
           }
           update_property(this->current_temperature, CalculateTemp(RXData[RX_C0_BYTE_T1_TEMP]), need_publish);
-          if ((fabs(this->current_temperature - CalculateTemp(RXData[RX_C0_BYTE_T2A_TEMP])) > 5.0) ||
-              this->defrost_sensor_) {
-            // Compressor running or we are in defrost.
+          if (fabs(this->current_temperature - CalculateTemp(RXData[RX_C0_BYTE_T2A_TEMP])) > 5.0) {
+            // Compressor running
             if ((this->mode == climate::CLIMATE_MODE_HEAT) && (RXData[9] & 0x0F) != 0x00) {
               this->action = climate::CLIMATE_ACTION_HEATING;
               need_publish = true;
